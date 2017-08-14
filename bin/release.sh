df3426bf40fa630438074c66e04c54770b27c874
Sebastian Tello <stello@atlassian.com>
2017-08-14 12:59:06 +1000

    chore: release script

---
+++ bin/release.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+version=`cat $DIR/version_to_release`
+
+format="^[0-9]+\\.[0-9]+\\.[0-9]+$"
+
+if ! [[ $version =~ $format ]]; then
+    echo "Invalid format in release version: $version"
+    exit 1
+fi
+
+echo "Preparing release of wiremock-pact-generator-$version"
+
+mvn --version
+mvn --batch-mode release:prepare -Dtag=wiremock-pact-generator-${version} -DreleaseVersion=${version}
+
+if [ $? -eq 0 ]; then
+    set -e
+    mvn release:perform
+else
+    mvn release:rollback
+    exit 1
+fi
